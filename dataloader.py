7cd1465d9d0114cb688772c2c35c5cc1cc135b85
Anton <whafu@kth.se>
2023-01-08 17:23:53 +0100

    Implement dataloader

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,8 @@ import numpy as np
 import PIL
 from PIL import Image
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
 
 LENA_IMAGE_PATH = r"data/lena_color.tiff"
 BABOON_IMAGE_PATH = r"data/baboon.png"
@@ -10,8 +12,8 @@ class dataloader():
     def __init__(self):
         pass
 
-    def get_data(self, data_name, show=False):
-        if data_name == "lena":
+    def get_data(self, data_name, blob_size=None, show=False):
+        if data_name=="lena":
             lena_img = Image.open(LENA_IMAGE_PATH)
             lena = np.array(lena_img)
             if show:
@@ -19,17 +21,25 @@ class dataloader():
                 plt.show()
             return lena
 
-        elif data_name == "baboon":
-            lena_img = Image.open(BABOON_IMAGE_PATH)
-            lena = np.array(lena_img)
+        elif data_name=="baboon":
+            baboon_img = Image.open(BABOON_IMAGE_PATH)
+            baboon = np.array(baboon_img)
             if show:
-                plt.imshow(lena)
+                plt.imshow(baboon)
                 plt.show()
-            return lena
+            return baboon
+
+        elif data_name=="blob":
+            coords, labels = datasets.make_blobs(n_samples=blob_size, centers=3, n_features=2, cluster_std=2, center_box=(-20, 20), random_state=42)
+            sns.scatterplot(coords[:,0], coords[:,1], labels)
+            if show:
+                plt.grid()
+                plt.show()
+            return coords, labels
 
 def main():
     dl = dataloader()
-    data = dl.get_data("baboon", show=True)
+    data = dl.get_data("blob", blob_size=10000 ,show=True)
 
 if __name__ == '__main__':
     main()
