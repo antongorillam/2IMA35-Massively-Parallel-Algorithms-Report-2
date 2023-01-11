27e45888c1222c91602b035e52526502981d00df
Anton <whafu@kth.se>
2023-01-11 22:47:22 +0100

    Small changes

--- coresetconstruction.py
+++ coresetconstruction.py
@@ -1,5 +1,5 @@
 from dataloader import Dataloader
-from kmeanspp import kplusplus
+from kmeanspp import kmeanspp
 from sklearn.metrics.pairwise import euclidean_distances
 import numpy as np
 
@@ -11,15 +11,25 @@ def coreset_construction(points, centers, epsilon=0.05):
     distances = euclidean_distances(points, centers)
     r = np.sqrt(np.sum(np.power(distances, 2))/(a*np.log(len(points))*len(points)))
     closest_centers = np.argmin(distances, 1)
+    num_points = 0
+
+    # I formulated point_weight s.t. its:
+    #   key = cord of the center of the grid
+    #   value = weight 
     for j in range(1):
         s = epsilon * 2**j * r / (np.sqrt(points.shape[1]))
         for i, (point, center_index) in enumerate(zip(points, closest_centers)):
+            num_points += 1
             c = centers[center_index]
-            grid_position = np.floor((point - c)/s)
+            grid_position = tuple(np.floor((point - c)/s))
 
             if grid_position in point_weights:
-                point_weights[grid_position][1] += 1
+                point_weights[grid_position] += 1
+            else: 
+                point_weights[grid_position] = 1
 
+    print(f"num_points: {num_points}")
+    return point_weights
 
 
     print(closest_centers)
@@ -30,7 +40,7 @@ def main():
     dl = Dataloader()
     coords, k = dl.get_data("blob", blob_size=10 ,show=False)
     centers, indices = kplusplus(coords, k, show=False)
-    coreset_construction(coords, centers)
+    point_weights = coreset_construction(coords, centers)
 
 if __name__ == '__main__':
     main()
