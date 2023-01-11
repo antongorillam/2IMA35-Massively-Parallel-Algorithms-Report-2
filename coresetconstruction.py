e4ff2af1a98f58bc21d3b48a0914ed7dce96b5e9
jonasbrondum <joni_brondum@protonmail.com>
2023-01-11 12:30:33 +0100

    Added a way to calculate the grid position of each point

--- coresetconstruction.py
+++ coresetconstruction.py
@@ -3,14 +3,26 @@ from kmeanspp import kplusplus
 from sklearn.metrics.pairwise import euclidean_distances
 import numpy as np
 
-def coresetConstruction(points, centers, epsilon=0.05):
+a = 1
+
+def coreset_construction(points, centers, epsilon=0.05):
     S = []
+    point_weights = dict()
     distances = euclidean_distances(points, centers)
+    r = np.sqrt(np.sum(np.power(distances, 2))/(a*np.log(len(points))*len(points)))
+    closest_centers = np.argmin(distances, 1)
+    for j in range(1):
+        s = epsilon * 2**j * r / (np.sqrt(points.shape[1]))
+        for i, (point, center_index) in enumerate(zip(points, closest_centers)):
+            c = centers[center_index]
+            grid_position = np.floor((point - c)/s)
 
-    r = np.sqrt(np.sum(np.power(distances, 2))/(np.log(len(points)*len(points))))
+            if grid_position in point_weights:
+                point_weights[grid_position][1] += 1
 
-    closestCenters = np.argmin(distances, 1)
-    print(closestCenters)
+
+
+    print(closest_centers)
     print(r)
 
 
@@ -18,7 +30,7 @@ def main():
     dl = Dataloader()
     coords, k = dl.get_data("blob", blob_size=10 ,show=False)
     centers, indices = kplusplus(coords, k, show=False)
-    coresetConstruction(coords, centers)
+    coreset_construction(coords, centers)
 
 if __name__ == '__main__':
     main()
