82bae3b9b5c6c2b7a48b2b3b87fb521a304f0ecb
Anton <whafu@kth.se>
2023-01-15 21:56:33 +0100

    experiment 3 works

--- coresetconstruction.py
+++ coresetconstruction.py
@@ -17,7 +17,7 @@ N_SAMPLES_LIST = [1000, 5000, 10000, 50000, 100000, 500000, 1000000]
 EPSILON_LIST = [1e-3, 1e-2, 0.05, 1e-1, 0.5]
 K_LIST = [3, 4, 5, 6, 7, 8]
 k = 3
-NUM_MACHINES = [16, 8, 4, 2]
+NUM_MACHINES = [4, 2]
 # EPSILON = 0.1
 
 PLOT_FOLDER = "plots"
@@ -155,9 +155,11 @@ def blob_clustering(points_weights, k=k, epsilon=0.1, mode="non-parallel", show=
         plt.savefig(file_name)
         # plt.show()
         # plt.close()
+        return tot_time
 
-def image_segmentation(image_array, mode="non-parallel" ,k=k, epsilon=0.1, show=False):
+def image_segmentation(image_array, mode="non-parallel" ,k=k, epsilon=0.1, show=False, path=None):
     # Reshaping the image into a 2D array of pixels and 3 color values (RGB)
+    time_start = time.time()
     pixel_vals = image_array.reshape((-1, 3))
 
     # Convert to float type
@@ -170,7 +172,9 @@ def image_segmentation(image_array, mode="non-parallel" ,k=k, epsilon=0.1, show=
         coreset_points, coreset_weights = coreset_construction(pixel_vals, weights, initial_centers)
     elif mode=="parallel":
         coreset_points, coreset_weights = coreset_construction_parallel(pixel_vals)
-    
+    elif mode=="no-coreset":
+        pass
+
     kmeans_coreset = KMeans(n_clusters=k, random_state=42).fit(coreset_points)
     coreset_centers = kmeans_coreset.cluster_centers_
 
@@ -180,9 +184,12 @@ def image_segmentation(image_array, mode="non-parallel" ,k=k, epsilon=0.1, show=
 
     # reshape data into the original image dimensions
     segmented_image = segmented_data.reshape(image_array.shape).astype(int)
-
+    total_time = time.time() - time_start
+    file_title = f"epsilon={epsilon}_k={k}_mode={mode}_totaltime={total_time:.1f}"
+    plt.title(file_title)
     plt.imshow(segmented_image)
-    plt.show()
+    plt.savefig(path + file_title + ".png")
+    return total_time
 
 def run_coreset_construction(i, points_weights, k=k, epsilon=0.1):
     points_weights = np.array(list(points_weights))[0] # Needed for parallelization
@@ -218,7 +225,9 @@ def coreset_construction_parallel(coords):
         # Merge every adjecent
         coreset = np.array([np.concatenate([points[i][1], points[i+1][1]], axis=0) for i in range(0, len(points), 2)])
 
-    coreset = coreset[0] 
+    rdd = sc.parallelize(coreset, 1)
+    coreset = rdd.mapPartitionsWithIndex(run_coreset_construction, preservesPartitioning=True).collect()
+    coreset = coreset[0][1] 
     d = coreset.shape[1] - 1
     S, S_weights = coreset[:,:d], coreset[:,d:d+1]
     return S, S_weights
@@ -228,7 +237,7 @@ def experiment_1():
     epsilon_list = [1e-3] #, 1e-2, 0.05, 1e-1, 0.5]
     k_list = [3] #, 4, 5, 6, 7, 8]
     k = 3
-    PLOT_FOLDER = "images/"
+    PLOT_FOLDER = "images/experiment_1/"
     for n in n_samples_list:
         for epsilon in epsilon_list:
             for k in k_list:
@@ -241,19 +250,60 @@ def experiment_2():
     pass
     
 def experiment_3():
-    # TODO: Anton will design this experiment for parallel vs non-parallel
-    pass
+    n_samples_list = [1000] #, 5000, 10000, 50000, 100000, 500000, 1000000]
+    epsilon_list = [1e-3] #, 1e-2, 0.05, 1e-1, 0.5]
+    k_list = [3, 4] #, 5, 6, 7, 8]
+    df = pd.DataFrame(
+        columns=["n_samples_list", "epsilon", "k", "mode", "execution time", "number of machines"]
+    )
+    PLOT_FOLDER = "images/experiment_3/"
+    for n in n_samples_list:
+        for epsilon in epsilon_list:
+            for k in k_list:
+                dl = Dataloader()
+                coords, _ = dl.get_data("blob", k=k, blob_size=n, show=False)
+                tot_time_parallel = blob_clustering(coords, epsilon=epsilon, k=k, mode="parallel", show=True, path=PLOT_FOLDER)
+                tot_time_normal = blob_clustering(coords, epsilon=epsilon, k=k, mode="non-parallel", show=True, path=PLOT_FOLDER)
+                df = df.append(
+                    {"n":n ,"epsilon":epsilon, "k":k, "mode":"parallel", "execution time":tot_time_parallel, "number of machines":NUM_MACHINES[0]},
+                    ignore_index=True)
+                df = df.append(
+                    {"n":n ,"epsilon":epsilon, "k":k, "mode":"non_parallel", "execution time":tot_time_normal, "number of machines":NUM_MACHINES[0]},
+                    ignore_index=True)
+
+    df.to_csv(PLOT_FOLDER + "performance_data.csv")
 
 def experiment_4():
-    # TODO: Anton will design this experiment for coreset vs non-coreset (maybe on image segmantion) 
-    
-    pass
+    # Experiment for coreset vs non-coreset (maybe on image segmantion) 
+    epsilon_list = [1e-3] #, 1e-2, 5e-2, 1e-1, 5e-1]
+    k_list = [2,3]
+    DATASET_NAME = ["lena", "baboon"]
+    df = pd.DataFrame(
+        columns=["dataset", "epsilon", "k", "mode", "execution time", "number of machines"]
+    )
+    # TODO: Wrong, it should be non-coreset vs coreset, not parallel, vs non-parallel
+    for dataset in DATASET_NAME:
+        PLOT_FOLDER = f"images/experiment_4/{dataset}_"
+        for epsilon in epsilon_list:
+            for k in k_list:
+                dl = Dataloader()
+                coords, _ = dl.get_data(dataset, k=k, blob_size=n, show=False)
+                tot_time_parallel = image_segmentation(coords, epsilon=epsilon, k=k, mode="parallel", show=True, path=PLOT_FOLDER)
+                tot_time_normal = image_segmentation(coords, epsilon=epsilon, k=k, mode="non-parallel", show=True, path=PLOT_FOLDER)
+                df = df.append(
+                    {"dataset":dataset ,"epsilon":epsilon, "k":k, "mode":"parallel", "execution time":tot_time_parallel, "number of machines":NUM_MACHINES[0]},
+                    ignore_index=True)
+                df = df.append(
+                    {"dataset":dataset ,"epsilon":epsilon, "k":k, "mode":"non_parallel", "execution time":tot_time_normal, "number of machines":NUM_MACHINES[0]},
+                    ignore_index=True)
+
+    df.to_csv(PLOT_FOLDER + "performance_data.csv")
 
 def main():
     # experiment_1()
     # experiment_2()
-    # experiment_3()
-    experiment_4()
+    experiment_3()
+    # experiment_4()
 
 if __name__ == '__main__':
     main()
