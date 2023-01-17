476f6dfc241327c21a72a94901a562f3be64048e
Anton <whafu@kth.se>
2023-01-17 12:44:24 +0100

    Delete unnecessary files and refractor

--- plotting.py
+++ plotting.py
@@ -8,7 +8,7 @@ def plot_experiment_1():
 def plot_experiment_2():
     pass
 
-def plot_experiment_3():
+def plot_experiment_2():
     experiment_path = r"images/experiment_3/"
 
     epsilon = "1e-1"
@@ -16,7 +16,7 @@ def plot_experiment_3():
     epsilon_df = df[df["epsilon"]==float(epsilon)]
     sns.set_style("white")
     fig = sns.lineplot(data=epsilon_df, x="n_samples", y="execution time", hue="mode")
-    fig.set_title(f"Time vs Sample Size: Bloobs\nepsilon {epsilon}, k 3")
+    fig.set_title(f"Time vs Sample Size: Blobs\nepsilon {epsilon}, k 3")
     plt.grid()
     plt.savefig(experiment_path + f"time_vs_n_epsilon={epsilon}_k=3.png")
     plt.close()
@@ -25,13 +25,13 @@ def plot_experiment_3():
     epsilon_df = df[df["epsilon"]==float(epsilon)]
     sns.set_style("white")
     fig = sns.lineplot(data=epsilon_df, x="n_samples", y="execution time", hue="mode")
-    fig.set_title(f"Time vs Sample Size: Bloobs\nepsilon {epsilon}, k 15")
+    fig.set_title(f"Time vs Sample Size: Blobs\nepsilon {epsilon}, k 15")
     plt.grid()
     plt.savefig(experiment_path + f"time_vs_n_epsilon={epsilon}_k=15.png")
     plt.close()
 
 
-def plot_experiment_4():
+def plot_experiment_3():
     experiment_path = r"images/experiment_4/"
 
     # epsilon = "1e-2"
@@ -59,7 +59,6 @@ def main():
     # plot_experiment_1()
     # plot_experiment_2()
     # plot_experiment_3() 
-    plot_experiment_4()
 
 if __name__ == '__main__':
     main()
